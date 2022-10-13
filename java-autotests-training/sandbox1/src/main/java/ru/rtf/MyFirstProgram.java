7982a9367380ca80b84a30fc69342f4e4bbeef69
jsus <jsus25@gmail.com>
2022-10-13 11:40:57 +0700

    добавлен класс точек на плоскости добавлен рассчет расстояния между двумя точками

--- java-autotests-training/sandbox1/src/main/java/ru/rtf/MyFirstProgram.java
+++ java-autotests-training/sandbox1/src/main/java/ru/rtf/MyFirstProgram.java
@@ -3,7 +3,21 @@ package ru.rtf;
 public class MyFirstProgram{
 
   public static void main(String[] args) {
-    System.out.println("Hello, Julia!");
+    hello("Julia");
+    hello("everybody");
+
+    Point p1 = new Point(1, 1);
+    Point p2 = new Point(4, 5);
+    System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") " +
+            "равно " + dimention(p1, p2));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double dimention(Point p1, Point p2) {
+    return (Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x)));
   }
 
 }

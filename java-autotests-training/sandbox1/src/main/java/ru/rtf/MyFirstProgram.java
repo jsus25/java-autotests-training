b9ae05bbec5cf284618abe4dfa0797660ae222bc
jsus <jsus25@gmail.com>
2022-10-13 15:25:33 +0700

    функция рассчета расстояния между двумя точками вынесена в метод класса

--- java-autotests-training/sandbox1/src/main/java/ru/rtf/MyFirstProgram.java
+++ java-autotests-training/sandbox1/src/main/java/ru/rtf/MyFirstProgram.java
@@ -1,6 +1,6 @@
 package ru.rtf;
 
-public class MyFirstProgram{
+public class MyFirstProgram {
 
   public static void main(String[] args) {
     hello("Julia");
@@ -9,15 +9,17 @@ public class MyFirstProgram{
     Point p1 = new Point(1, 1);
     Point p2 = new Point(4, 5);
     System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") " +
-            "равно " + dimention(p1, p2));
+            "равно " + Point.distance(p1, p2));
   }
+
+//  public static double distance(Point p1, Point p2) {
+//    return (Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x)));
+//  }
+
 
   public static void hello(String somebody) {
     System.out.println("Hello " + somebody + "!");
   }
 
-  public static double dimention(Point p1, Point p2) {
-    return (Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x)));
-  }
 
 }

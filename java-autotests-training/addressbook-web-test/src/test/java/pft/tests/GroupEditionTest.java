1878c6aecf40ccc9d5fc8ef9ef335441833860cd
jsus <jsus25@gmail.com>
2022-11-07 21:00:09 +0700

    В тесте редактирования группы добавлено ее создание при отсутствии групп

--- java-autotests-training/addressbook-web-test/src/test/java/pft/tests/GroupEditionTest.java
+++ java-autotests-training/addressbook-web-test/src/test/java/pft/tests/GroupEditionTest.java
@@ -10,6 +10,9 @@ public class GroupEditionTest extends TestBase{
   @Test
   public void testGroupEdition() {
     app.getNavigationHelper().goToGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("group5", "h5", "f5"));
+    }
     app.getGroupHelper().selectElement();
     app.getGroupHelper().initEdition();
     app.getGroupHelper().fillGroupForm(new GroupData("groop7", "gh10", "gf10"));

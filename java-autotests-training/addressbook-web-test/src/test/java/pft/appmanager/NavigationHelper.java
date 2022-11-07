1e03942d19786221138ed8aded45c148c77eb575
jsus <jsus25@gmail.com>
2022-11-07 13:20:40 +0700

    Доработаны методы перехода по меню: добавлена проверка, что мы не находимся еще на этой странице

--- java-autotests-training/addressbook-web-test/src/test/java/pft/appmanager/NavigationHelper.java
+++ java-autotests-training/addressbook-web-test/src/test/java/pft/appmanager/NavigationHelper.java
@@ -10,14 +10,23 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void goToGroupPage() {
+    if (isElementPresent(By.name("new")) && driver.findElement(By.tagName("h1")).getText().equals("Groups")) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void goToContactAddPage() {
+    if (isElementPresent(By.linkText("Enter"))) {
+      return;
+    }
     click(By.linkText("add new"));
   }
 
   public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 }

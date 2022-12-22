85063fade96b8297ca26f2f655436d8d730740d2
jsus <jsus25@gmail.com>
2022-12-22 12:57:12 +0700

    Убрали из сравнения списков групп лишний код - цикл по элементам. Теперь сравниваем просто списки

--- java-autotests-training/addressbook-web-test/src/test/java/pft/tests/GroupDeletionTest.java
+++ java-autotests-training/addressbook-web-test/src/test/java/pft/tests/GroupDeletionTest.java
@@ -22,8 +22,6 @@ public class GroupDeletionTest extends TestBase {
     Assert.assertEquals(after.size(), before.size() - 1);
 
     before.remove(before.size() - 1);
-    for (int i = 0; i < after.size(); i++){
-      Assert.assertEquals(after.get(i), before.get(i));
-    }
+    Assert.assertEquals(after, before);
   }
 }

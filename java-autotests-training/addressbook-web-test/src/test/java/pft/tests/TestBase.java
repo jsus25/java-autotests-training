5d7d2e3f7ce2e449ced7ed58f41cf1ad57b024fb
jsus <jsus25@gmail.com>
2022-12-27 16:31:56 +0700

    запуск и останов браузера переопределен на начало и конец ВСЕХ тестов

--- java-autotests-training/addressbook-web-test/src/test/java/pft/tests/TestBase.java
+++ java-autotests-training/addressbook-web-test/src/test/java/pft/tests/TestBase.java
@@ -1,20 +1,20 @@
 package pft.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import pft.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager("CHROME");
+  protected static final ApplicationManager app = new ApplicationManager("CHROME");
   //EDGE, FIREFOX, CHROME
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }

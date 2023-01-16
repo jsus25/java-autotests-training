ec0952c477400ac18ed6ead6ed884286e6247f6c
jsus <jsus25@gmail.com>
2023-01-16 13:19:06 +0700

    6.2. Реализован генератор файла с данными для групп

---
+++ java-autotests-training/addressbook-web-test/src/test/java/pft/generators/GroupDataGenerators.java
@@ -0,0 +1,33 @@
+package pft.generators;
+
+import pft.model.GroupData;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupDataGenerators {
+  public static void main(String[] args) throws IOException {
+    int count = Integer.parseInt(args[0]);
+    File file = new File(args[1]);
+    List<GroupData> groups = generateGroups(count);
+    save(groups, file);
+  }
+
+  private static List<GroupData> generateGroups(int count) {
+    List<GroupData> groups = new ArrayList<>();
+    for (int i = 0; i < count; i++) {
+      GroupData group = new GroupData(null, String.format("group %s",i),String.format("header %s",i), String.format("footer %s",i));
+      groups.add(group);
+    }
+    return groups;
+  }
+  private static void save(List<GroupData> groups, File file) throws IOException {  //IO IOException - добавляется идеей при выборе того,
+                                                                                    // как обрабатывать исключения в случае, если не будет возможности открыть файл на запись
+    Writer writer = new FileWriter(file);  //открыть файл на запись
+    for (GroupData group: groups) {
+      writer.write(String.format("%s;%s;%s\n", group.name(), group.header(), group.footer()));
+    }
+    writer.close();    //обязательно закрыть файл
+  }
+
+}

a75bc6884381f41a615cb5c35456cd8602a76347
jsus <jsus25@gmail.com>
2023-01-17 20:37:53 +0700

    6.7. Реализована генерация тестовых данных в json и их использование в тесте по созданию групп

--- java-autotests-training/addressbook-web-test/src/test/java/pft/generators/GroupDataGenerator.java
+++ java-autotests-training/addressbook-web-test/src/test/java/pft/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package pft.generators;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
 import pft.model.GroupData;
 import java.io.*;
 import java.util.ArrayList;
@@ -9,8 +12,14 @@ public class GroupDataGenerator {
   public static void main(String[] args) throws IOException {
     int count = Integer.parseInt(args[0]);
     File file = new File(args[1]);
+    String format = args[1].substring(args[1].lastIndexOf(".") + 1);
     List<GroupData> groups = generateGroups(count);
-    save(groups, file);
+    switch (format) {
+      case "csv" -> saveAsCsv(groups, file);
+      case "xml" -> saveAsXml(groups, file);
+      case "json" -> saveAsJson(groups, file);
+      default -> System.out.println("Unknown file formal");
+    }
   }
 
   private static List<GroupData> generateGroups(int count) {
@@ -21,7 +30,7 @@ public class GroupDataGenerator {
     }
     return groups;
   }
-  private static void save(List<GroupData> groups, File file) throws IOException {  //IO IOException - добавляется идеей при выборе того,
+  private static void saveAsCsv(List<GroupData> groups, File file) throws IOException {  //IO IOException - добавляется идеей при выборе того,
                                                                                     // как обрабатывать исключения в случае, если не будет возможности открыть файл на запись
     Writer writer = new FileWriter(file);  //открыть файл на запись
     for (GroupData group: groups) {
@@ -29,5 +38,22 @@ public class GroupDataGenerator {
     }
     writer.close();    //обязательно закрыть файл
   }
+  private static void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
+
+  private static void saveAsXml(List<GroupData> groups, File file) throws IOException {
+    XStream xStream = new XStream();
+    xStream.alias("group", GroupData.class);   //необязательное определение названия полей, чтобы красиво называлось
+    String xml = xStream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
 
 }

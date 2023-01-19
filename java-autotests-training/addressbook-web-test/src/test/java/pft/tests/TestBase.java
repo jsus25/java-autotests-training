7203ec55e3482a47cf0c3418a55827d1713ee420
jsus <jsus25@gmail.com>
2023-01-19 13:50:06 +0700

    6.11. Добавлено логирование в консоль и в файл

--- java-autotests-training/addressbook-web-test/src/test/java/pft/tests/TestBase.java
+++ java-autotests-training/addressbook-web-test/src/test/java/pft/tests/TestBase.java
@@ -1,25 +1,44 @@
 package pft.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pft.appmanager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", "CHROME"));
                                                                  //EDGE, FIREFOX, CHROME
+
 
   @BeforeSuite
   public void setUp() throws IOException {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
   }
 
 }

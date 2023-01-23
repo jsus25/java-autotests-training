6dede61b88a9a9597db2d65d48e7a6848b794b99
jsus <jsus25@gmail.com>
2023-01-23 14:54:02 +0700

    7.2. Настроен коннект с БД через Hibernate.Смаплены таблицы групп.

--- java-autotests-training/addressbook-web-test/src/test/java/pft/model/GroupData.java
+++ java-autotests-training/addressbook-web-test/src/test/java/pft/model/GroupData.java
@@ -1,8 +1,39 @@
 package pft.model;
 
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
-public record GroupData(Integer id, String name, String header, String footer) {
+@Entity                           //для hibernate
+@Table(name = "group_list")          //для hibernate
+public final class GroupData {
+  @Id                             //для hibernate
+  @Column(name = "group_id")      //для hibernate
+  private Integer id;
+  @Column(name = "group_name")
+  private String name;
+  @Column(name = "group_header")
+  @Type(type = "text")              //для hibernate
+  private String header;
+  @Column(name = "group_footer")
+  @Type(type = "text")              //для hibernate
+  private String footer;
+
+  public GroupData(Integer id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public GroupData() {
+
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -15,4 +46,30 @@ public record GroupData(Integer id, String name, String header, String footer) {
   public int hashCode() {
     return Objects.hash(id, name);
   }
+
+  public Integer id() {
+    return id;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public String header() {
+    return header;
+  }
+
+  public String footer() {
+    return footer;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData[" +
+            "id=" + id + ", " +
+            "name=" + name + ", " +
+            "header=" + header + ", " +
+            "footer=" + footer + ']';
+  }
+
 }

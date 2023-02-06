e79447b5f8a3b81d0a534ef1274b435e8f88e1dd
jsus <jsus25@gmail.com>
2023-02-06 10:08:37 +0700

    8.4. Реализована "ленивая" инициализация вебдрайвера и помощника по регистрации - только тогда, когда они становятся нужны

--- java-autotests-training/mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ java-autotests-training/mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -14,11 +14,10 @@ import java.util.Properties;
 
 public class ApplicationManager {
   private final Properties properties;
-  protected WebDriver driver;
-//  private NavigationHelper navigationHelper;
-
-  JavascriptExecutor js;
+  private WebDriver driver;
   private final String browser;
+  JavascriptExecutor js;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -28,22 +27,12 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
-
-    if (Objects.equals(browser, "CHROME")) {
-      driver = new ChromeDriver();
-    } else if (Objects.equals(browser, "FIREFOX")) {
-      driver = new FirefoxDriver();
-    } else if (Objects.equals(browser, "EDGE")) {
-      driver = new EdgeDriver();
-    }
-
-    js = (JavascriptExecutor) driver;
-    driver.get(properties.getProperty("web.baseUrl"));
-    driver.manage().window().setSize(new Dimension(1198, 804));
   }
 
   public void stop() {
-    driver.quit();
+    if (driver != null) {
+      driver.quit();
+    }
   }
 
   public HttpSession newSession() {
@@ -52,5 +41,29 @@ public class ApplicationManager {
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (driver == null) {
+      if (Objects.equals(browser, "CHROME")) {
+        driver = new ChromeDriver();
+      } else if (Objects.equals(browser, "FIREFOX")) {
+        driver = new FirefoxDriver();
+      } else if (Objects.equals(browser, "EDGE")) {
+        driver = new EdgeDriver();
+      }
+
+      js = (JavascriptExecutor) driver;
+      driver.get(properties.getProperty("web.baseUrl"));
+      driver.manage().window().setSize(new Dimension(1198, 804));
+    }
+    return driver;
   }
 }

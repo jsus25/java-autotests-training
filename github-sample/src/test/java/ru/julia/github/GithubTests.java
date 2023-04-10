02e5c0bc985ba129d28d67164b35fc682623d4a9
jsus <jsus25@gmail.com>
2023-04-10 13:09:43 +0700

    private keys removed

--- github-sample/src/test/java/ru/julia/github/GithubTests.java
+++ github-sample/src/test/java/ru/julia/github/GithubTests.java
@@ -10,7 +10,7 @@ public class GithubTests {
 
   @Test
   public void testCommits() throws IOException {
-    Github github = new RtGithub("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");
+    Github github = new RtGithub("your access token");
     RepoCommits commits = github.repos().get(new Coordinates.Simple("jsus25", "java-autotests-training")).commits();
     for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
       System.out.println(new RepoCommit.Smart(commit).message());

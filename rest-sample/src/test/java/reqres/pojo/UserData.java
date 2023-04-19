67635e44415bc027fe510ffa40bfff276f4b23c7
jsus <jsus25@gmail.com>
2023-04-19 13:34:31 +0700

    Lombok connected to rest-sample module Selenium in addressbook module upgraded to the latest version

---
+++ rest-sample/src/test/java/reqres/pojo/UserData.java
@@ -0,0 +1,14 @@
+package reqres.pojo;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class UserData {
+  private Integer id;
+  private String email;
+  private String first_name;
+  private String last_name;
+  private String avatar;
+}
